f50d548eac732de97273d8db14ef57c905889530
SimonHiesl <simon.hiesl@gmail.com>
2023-01-30 16:17:04 +0100

    .

--- Cryptography/One-Time-Pad.py
+++ Cryptography/One-Time-Pad.py
@@ -1,5 +1,6 @@
 import numpy as np
 import secrets
+import random as rd
 
 characters = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '.', ',', '!', '?', ':', ';', "'", '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '=', '+', '-', '*', '/', '(', ')', '[', ']', '{', '}', '@', '§', '$', '%', '&', '~', '#', '_', '<', '>', '|', '^', 'ä', 'ö', 'ü', 'Ä', 'Ö', 'Ü', 'ß']
 
@@ -98,6 +99,13 @@ def secure_key(key_size):
     key = ""
     for i in range(key_size):
         key += str(secrets.randbelow(10))
+    return key
+
+def insecure_key(key_size):
+    rd.seed(0)
+    key = ""
+    for i in range(key_size):
+        key += str(rd.randint(0, 9))
     return key
 
 key_length = 100000

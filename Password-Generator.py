0dc5528b9b631490a3a3e5f965d4c3d3967dea83
f <socials.fl@icloud.com>
2023-01-30 12:54:48 +0100

    cli compatibilety

--- Password-Generator.py
+++ Password-Generator.py
@@ -1,5 +1,6 @@
 import secrets
 import math
+import sys
 
 ASCII = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '.', ',', '!', '?', ':', ';', "'", '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '=', '+', '-', '*', '/', '(', ')', '[', ']', '{', '}', '@', '§', '$', '%', '&', '~', '#', '_', '<', '>', '|', '^', 'ä', 'ö', 'ü', 'Ä', 'Ö', 'Ü', 'ß']
 Num_CS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -17,4 +18,19 @@ def create_password(pw_length, pw_signs):
     print(password, "\n")
     print("Strength:", bits, "bits")
 
-create_password(15, Num_CS)
+
+def help():  # help prints little help message
+    print("""
+    
+    pwgen [pwlen] [pw_signs]    arguments must be passed in correct order as shown
+                                [pwlen] is length of password as int
+                                [pw_signs] is chars of pw avalible are ASCII, Num_CS, CS, CI, Num, Bin, PAO
+    
+    """)
+
+# create_password(15, Num_CS)
+
+if len(sys.argv) = 4 and sys.argv[1] == "pwgen":
+    create_password(sys.argv[2], sysargv[3])
+else:
+    help()

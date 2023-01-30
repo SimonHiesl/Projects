2df09646894aec0b370a47f72b25ddb3eb265d10
f <socials.fl@icloud.com>
2023-01-30 13:55:57 +0100

    various bug fixses aand improvements

--- Password-Generator.py
+++ Password-Generator.py
@@ -10,27 +10,36 @@ Num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 Bin = ['0', '1']
 PAO = ['a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x']
 
-def create_password(pw_length, pw_signs):
-    password = ""
+def create_password(pw_length, pw_signs, password):
     bits = round(math.log(len(pw_signs),2)*pw_length,1)
     for i in range(pw_length):
-        password += str(secrets.choice(pw_signs))
-    print(password, "\n")
-    print("Strength:", bits, "bits")
-
+        password +=     str(secrets.choice(pw_signs))
+    return password, bits
 
 def help():  # help prints little help message
+    print(sys.argv[2].isdigit())
     print("""
     
-    pwgen [pwlen] [pw_signs]    arguments must be passed in correct order as shown
-                                [pwlen] is length of password as int
-                                [pw_signs] is chars of pw avalible are ASCII, Num_CS, CS, CI, Num, Bin, PAO
+        pwgen [pwlen] [pw_signs]    arguments must be passed in correct order as shown
+                                    [pwlen] is length of password as int
+                                    [pw_signs] either use one of the avalible pw_sing lists or pass allowed chars your self
+                                        avalible pw_sign lists are ASCII, Num_CS, CS, CI, Num, Bin, PAO
     
     """)
 
-# create_password(15, Num_CS)
+if( len(sys.argv) == 4 
+    and sys.argv[1] == "pwgen" # check count of passed arguments
+    and sys.argv[1] == "pwgen"  # check for first argument beeing "pwgen"
+    and sys.argv[2].isdigit() ): # check for second argument beeing a number 
+    
+    password, bits = create_password( int(sys.argv[2]),  # first argument is pw length as int
+                                      eval(sys.argv[3]) if sys.argv[3] in ['ASCII', 'Num_CS', 'CS', 'CI', 'Num', 'Bin', 'PAO'] else sys.argv[3],  # seconde arument is char set to use for pw
+                                      "")  # third argument is string to initialise password variable
+    
+    print(f"""
+        Password:  {password}
+        Strength:  {bits} bits
+    """)
 
-if len(sys.argv) = 4 and sys.argv[1] == "pwgen":
-    create_password(sys.argv[2], sysargv[3])
 else:
     help()
